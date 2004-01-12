1dce67bdbd7337b20f6de56507e7a2461fa3b9c2
Martin Maechler <maechler@r-project.org>
2004-01-12 10:40:53 +0000

    version of 2004.01.12.10.40.53 (now is 06 Sep 2018).

--- tests/o-linux.R
+++ tests/o-linux.R
@@ -1,25 +1,30 @@
+### Very similar to ./ex.R  but using *MORE* precision
+###  ===> non-portable but better for consistency checking (Development)
 library(fracdiff)
 
 .proctime00 <- proc.time()
-
 set.seed(107)
+options(digits = 10)
+
+## 1)
+
 x1 <-  fracdiff.sim( 5000, ar = .2, ma = -.4, d = .3)
-fd1 <- fracdiff(x1$series, nar = 1, nma = 1, dtol = 1e-10)
+(fd1 <- fracdiff(x1$series, nar = 1, nma = 1, dtol = 1e-10))
 
+fdCOVcomp <-
+    c("h", "covariance.dpq", "stderror.dpq", "correlation.dpq", "hessian.dpq")
 fd1. <- fracdiff.var(x1$series, fd1, h = fd1$h / 8)
-fd1.[c("h", "covariance.dpq", "stderror.dpq", "correlation.dpq", "hessian.dpq")]
-
+fd1.[fdCOVcomp]
 fd1u <- fracdiff.var(x1$series, fd1, h = fd1$h * 8)
-fd1u[c("h", "covariance.dpq", "stderror.dpq", "correlation.dpq", "hessian.dpq")]
+sapply(fd1u[fdCOVcomp], signif, digits= 8)
+
+## 2)
 
 x2 <-  fracdiff.sim( 2048, ar = .8, ma = -.4, d = .3)# -> NA's and problems
 (fd2 <- fracdiff(x2$series, nar = length(x2$ar), nma = length(x2$ma)))
 
-fd2. <- fracdiff.var(x2$series, fd2, h = fd2$h / 8)
-fd2.[c("h", "covariance.dpq", "stderror.dpq", "correlation.dpq", "hessian.dpq")]
-
-fd2u <- fracdiff.var(x2$series, fd2, h = fd2$h * 8)
-fd2u[c("h", "covariance.dpq", "stderror.dpq", "correlation.dpq", "hessian.dpq")]
+(fd2. <- fracdiff.var(x2$series, fd2, h = fd2$h / 8))[fdCOVcomp]
+(fd2u <- fracdiff.var(x2$series, fd2, h = fd2$h * 8))[fdCOVcomp]
 
 ## Last Line:
 cat('Time elapsed: ', proc.time() - .proctime00,'\n')
