e9f287b27a076c1451dbb18a3252ff4933063639
Martin Maechler <maechler@r-project.org>
2004-05-31 23:49:37 +0000

    Initial revision

---
+++ src/pmult.c
@@ -0,0 +1,28 @@
+/*
+ *  Copyright (C) 2004 Martin Maechler
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  A copy of the GNU General Public License is available via WWW at
+ *  http://www.gnu.org/copyleft/gpl.html.  You can also obtain it by
+ *  writing to the Free Software Foundation, Inc., 59 Temple Place,
+ *  Suite 330, Boston, MA  02111-1307  USA.
+ */
+
+#include <R.h>
+#include <Rinternals.h>
+
+SEXP poly_mult(SEXP a, SEXP b)
+{
+    SEXP prod;
+
+    return prod;
+}
