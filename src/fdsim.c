8388ebe75c28ca18b3d1496d33195cd4639fdd65
Martin Maechler <maechler@r-project.org>
2004-09-18 16:11:48 +0000

    version of 2004.09.18.16.11.48 (now is 06 Sep 2018).

--- src/fdsim.c
+++ src/fdsim.c
@@ -1,3 +1,6 @@
+/* Produced by
+ * $Id: f2c-clean,v 1.10 2002/03/28 16:37:27 maechler Exp $
+ */
 /* fdsim.f -- translated by f2c (version 20031025).
    You must link the resulting object file with libf2c:
 	on Microsoft Windows system, link with libf2c.lib;
@@ -8,68 +11,81 @@
 	Source for libf2c is in /netlib/f2c/libf2c.zip, e.g.,
 
 		http://www.netlib.org/f2c/libf2c.zip
-*/
 
-#include "f2c.h"
+
+-- be brave, try without >>>
+ * #include "f2c.h" /* <<<<< ------*/
+#include <math.h>
+#ifndef max
+# define	max(a, b) 		((a) < (b) ? (b) : (a))
+#endif
+#ifndef min
+# define	min(a, b)		((a) > (b) ? (b) : (a))
+#endif
+#ifndef abs
+# define	abs(x)			((x) >= 0 ? (x) : -(x))
+#endif
+
 
 /* Common Block Declarations */
 
 struct {
-    doublereal fltmin, fltmax, epsmin, epsmax;
+    double fltmin, fltmax, epsmin, epsmax;
 } machfd_;
 
 #define machfd_1 machfd_
 
 struct {
-    integer igamma, jgamma;
+    int igamma, jgamma;
 } gammfd_;
 
 #define gammfd_1 gammfd_
 
-/* Subroutine */ int fdsim_(integer *n, integer *ip, integer *iq, doublereal *
-	ar, doublereal *ma, doublereal *d__, doublereal *rmu, doublereal *y, 
-	doublereal *s, doublereal *flmin, doublereal *flmax, doublereal *
-	epmin, doublereal *epmax)
+/* Subroutine */ int fdsim_(int *n, int *ip, int *iq, double *
+	ar, double *ma, double *d__, double *rmu, double *y,
+
+	double *s, double *flmin, double *flmax, double *
+	epmin, double *epmax)
 {
     /* System generated locals */
-    integer i__1, i__2;
-    doublereal d__1;
+    int i__1, i__2;
+    double d__1;
 
     /* Builtin functions */
-    double sqrt(doublereal);
+    double sqrt(double);
 
     /* Local variables */
-    static integer i__, j, k;
-    static doublereal dj, vk, dk1, amk, sum, dk1d, temp;
-    extern doublereal dgamr_(doublereal *), dgamma_(doublereal *);
+    static int i__, j, k;
+    static double dj, vk, dk1, amk, sum, dk1d, temp;
+    extern double dgamr_(double *), dgamma_(double *);
 
-/*  generates a random time series for use with fracdf */
+/*  generates a random time series for use with fracdf 
 
-/*  Input : */
+  Input : 
 
-/*  n      integer  length of the time series */
-/*  ip     integer  number of autoregressive parameters */
-/*  iq     integer  number of moving average parameters */
-/*  ar     real    (ip) autoregressive parameters */
-/*  ma     real    (iq) moving average parameters */
-/*  d      real     fractional differencing parameter */
-/*  rmu    real     time series mean */
-/*  y      real    (n+iq) 1st n : normalized random numbers */
-/*  s      real    (n+iq) workspace */
+  n      int  length of the time series 
+  ip     int  number of autoregressive parameters 
+  iq     int  number of moving average parameters 
+  ar     float    (ip) autoregressive parameters 
+  ma     float    (iq) moving average parameters 
+  d      float     fractional differencing parameter 
+  rmu    float     time series mean 
+  y      float    (n+iq) 1st n : normalized random numbers 
+  s      float    (n+iq) workspace 
 
-/*  Output : */
+  Output : 
 
-/*  s      real   (n) the generated time series */
-/* ----------------------------------------------------------------------------- */
+  s      float   (n) the generated time series 
+ ----------------------------------------------------------------------------- 
 
-/*        Simulates a series of length n from an ARIMA (p,d,q) model */
-/*        with fractional d (0 < d < 0.5). */
+        Simulates a series of length n from an ARIMA (p,d,q) model 
+        with fractional d (0 < d < 0.5). 
 
-/* ----------------------------------------------------------------------------- */
-/*     real               ar(ip), ma(iq), d, rmu */
-/*     real               y(n+iq), s(n+iq) */
-/* -------------------------------------------------------------------------- */
-    /* Parameter adjustments */
+ ----------------------------------------------------------------------------- 
+     float               ar(ip), ma(iq), d, rmu 
+     float               y(n+iq), s(n+iq) 
+ -------------------------------------------------------------------------- 
+     Parameter adjustments */
     --ar;
     --ma;
     --y;
@@ -87,8 +103,7 @@ struct {
     d__1 = 1. - *d__;
     temp = dgamr_(&d__1);
     if (gammfd_1.igamma != 0) {
-	i__1 = *n;
-	for (i__ = 1; i__ <= i__1; ++i__) {
+	for (i__ = 1; i__ <= *n; ++i__) { /* f2c-clean: s {i__1} {*n} */
 	    s[i__] = 0.;
 	}
 	return 0;
@@ -96,8 +111,7 @@ struct {
     d__1 = 1. - *d__ * 2.;
     vk = dgamma_(&d__1) * (temp * temp);
     if (gammfd_1.igamma != 0) {
-	i__1 = *n;
-	for (i__ = 1; i__ <= i__1; ++i__) {
+	for (i__ = 1; i__ <= *n; ++i__) { /* f2c-clean: s {i__1} {*n} */
 	    s[i__] = 0.;
 	}
 	return 0;
@@ -117,16 +131,14 @@ struct {
 
 /* 	 Generate y(3),...,y(n+iq) */
 
-    i__1 = *n + *iq;
-    for (k = 3; k <= i__1; ++k) {
-	dk1 = (doublereal) k - 1.;
+    for (k = 3; k <= (*n + *iq); ++k) { /* f2c-clean: s {i__1} {*n + *iq} */
+	dk1 = (double) k - 1.;
 	dk1d = dk1 - *d__;
 
 /* 	 Update the phi(j) using the recursion formula on W498 */
 
-	i__2 = k - 2;
-	for (j = 1; j <= i__2; ++j) {
-	    dj = dk1 - (doublereal) j;
+	for (j = 1; j <= (k - 2); ++j) { /* f2c-clean: s {i__2} {k - 2} */
+	    dj = dk1 - (double) j;
 	    s[j] *= dk1 * (dj - *d__) / (dk1d * dj);
 	}
 	temp = *d__ / dk1d;
@@ -139,8 +151,7 @@ struct {
 /* 	 Form amk */
 
 	amk = 0.;
-	i__2 = k - 1;
-	for (j = 1; j <= i__2; ++j) {
+	for (j = 1; j <= (k - 1); ++j) { /* f2c-clean: s {i__2} {k - 1} */
 	    amk += s[j] * y[k - j];
 	}
 
@@ -149,28 +160,24 @@ struct {
 	y[k] = amk + y[k] * sqrt(vk);
     }
 
-/* 	 We now have an ARIMA (0,d,0) realisation of length n+iq in */
-/* 	 y(k),k=1,n+iq. We now run this through an inverse ARMA(p,q) */
-/* 	 filter to get the final output in s(k), k=1,n. */
+/* 	 We now have an ARIMA (0,d,0) realisation of length n+iq in 
+ 	 y(k),k=1,n+iq. We now run this through an inverse ARMA(p,q) 
+ 	 filter to get the final output in s(k), k=1,n. */
 
-    i__1 = *n;
-    for (k = 1; k <= i__1; ++k) {
+    for (k = 1; k <= *n; ++k) { /* f2c-clean: s {i__1} {*n} */
 	sum = 0.;
 	j = min(*ip,k);
-	i__2 = j;
-	for (i__ = 1; i__ <= i__2; ++i__) {
+	for (i__ = 1; i__ <= j; ++i__) { /* f2c-clean: s {i__2} {j} */
 	    sum += ar[i__] * s[k - i__];
 	}
-	i__2 = *iq;
-	for (j = 1; j <= i__2; ++j) {
+	for (j = 1; j <= *iq; ++j) { /* f2c-clean: s {i__2} {*iq} */
 	    sum -= ma[j] * y[k + *iq - j];
 	}
 	s[k] = sum + y[k + *iq];
     }
 /* now add the global mean */
     if (*rmu != 0.) {
-	i__1 = *n;
-	for (i__ = 1; i__ <= i__1; ++i__) {
+	for (i__ = 1; i__ <= *n; ++i__) { /* f2c-clean: s {i__1} {*n} */
 	    s[i__] += *rmu;
 	}
     }
