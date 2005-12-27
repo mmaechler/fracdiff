f6aa0281d2383fa11b9e5c35d97cd209e025b3b5
Martin Maechler <maechler@r-project.org>
2005-12-27 23:22:50 +0000

    version of 2005.12.27.23.22.50 (now is 17 Jan 2014).

--- R/diffseries.R
+++ R/diffseries.R
@@ -8,18 +8,18 @@ diffseries <- function(x, d)
     if (any(is.na(x)))
         stop("NAs in x")
     n <- length(x)
+    stopifnot(n >= 2)
     x <- x - mean(x)
-    PI <- matrix(0, n, 1)
+    PI <- numeric(n)
     PI[1] <- -d
-    for (k in 2:n){
+    for (k in 2:n) {
         PI[k] <- PI[k-1]*(k - 1 - d)/k
     }
-    ydiff <- NULL
-    ydiff[1] <- x[1]
+    ydiff <- x
     for (i in 2:n) {
         ydiff[i] <- x[i] + sum(PI[1:(i-1)]*x[(i-1):1])
     }
-    res <- list(sdiff = ydiff)
-    res
+    ## return numeric!
+    ydiff
 }
 
