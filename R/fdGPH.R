c0e0281b04648c168d079125d208748e7a0c6b0e
Martin Maechler <maechler@r-project.org>
2006-02-02 23:16:41 +0000

    version of 2006.02.02.23.16.41 (now is 06 Sep 2018).

--- R/fdGPH.R
+++ R/fdGPH.R
@@ -1,29 +1,30 @@
+#### by Valderio Reisen  -- Dec.2005
+
+## MM(FIXME): This is "in parallel"  to Sperio() , see ./Sperio.R
+
 GPH <- function(x, bandw.exp = 0.5)
 {
-    x <- as.data.frame(x)
-    names(x) <- "series"
-    x <- x$series
-    if (NCOL(x) > 1)
-        stop("only implemented for univariate time series")
-    if (any(is.na(x)))
-        stop("NAs in x")
+    x <- as.numeric(na.fail(as.ts(x)))
+    if (any(is.na(x))) stop("NAs in x")
+    if (NCOL(x) > 1) stop("only implemented for univariate time series")
     n <- length(x)
-    g <- trunc(n^band)
+    ## Compute "smoothed" periodogram -- MM (FIXME): use  spec.pgram() !
+    g <- trunc(n^bandw.exp)
     j <- 1:g
     w <- 2*pi*j/n
-    var.x <- sum((x - mean(x))^2)/n # not /(n-1)
-    cov.x <- matrix(0, n-1, 1)
+    mx <- mean(x)
+    var.x <- sum((x - mx)^2)/n # not /(n-1)
+    cov.x <- numeric(n-1)
     for (k in 1:(n-1)) {
-        cov.x[k] <- (1/n)*sum((x[1:(n-k)] - mean(x))*(x[(1+k):n] - mean(x)))
+        cov.x[k] <- (1/n)*sum((x[1:(n-k)] - mx)*(x[(1+k):n] - mx))
     }
-    periodogram <- matrix(0, g, 1)
+    periodogram <- numeric(g)
     for (i in 1:g) {
         periodogram[i] <- (1/(2*pi))*(var.x + 2*sum(cov.x*cos(w[i]*(1:(n-1)))))
     }
-    y.reg <- log(periodogram[periodogram > 0])
-    x.reg <- log((2*sin(w/2))^2)
     pos <- j[periodogram > 0]
-    x.reg <- x.reg[pos]
+    y.reg <- log(periodogram[pos])
+    x.reg <- 2*log(2*sin(w[pos]/2)) ## = log( (2*sin(..)) ^ 2)
     fit <- lm(y.reg ~ x.reg)
     d.GPH <- coef(fit)[2]
     names(d.GPH) <- NULL
