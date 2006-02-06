68bcc85c7cc8a147f09dab9935411a191876920f
Martin Maechler <maechler@r-project.org>
2006-02-06 14:03:28 +0000

    version of 2006.02.06.14.03.28 (now is 06 Sep 2018).

--- R/fdSperio.R
+++ R/fdSperio.R
@@ -1,8 +1,8 @@
 #### by Valderio Reisen  -- Dec.2005
 
-## MM(FIXME): This is "in parallel"  to GPH() , see ./GPH.R
+## MM(FIXME): This is "in parallel"  to fdGPH() , see ./fdGPH.R
 
-Sperio <- function(x, bandw.exp = 0.5, beta = 0.9)
+fdSperio <- function(x, bandw.exp = 0.5, beta = 0.9)
 {
     x <- as.numeric(na.fail(as.ts(x)))
     if (any(is.na(x))) stop("NAs in x")
@@ -11,35 +11,36 @@ Sperio <- function(x, bandw.exp = 0.5, beta = 0.9)
     ## Compute "smoothed" periodogram -- MM (FIXME): use  spec.pgram() !
     g <- trunc(n^bandw.exp)
     j <- 1:g
+    kk <- 1:(n-1)
     w <- 2*pi*j/n
     mx <- mean(x)
     var.x <- sum((x - mx)^2)/n # not /(n-1)
-    cov.x <- matrix(0, n-1, 1)
-    for (k in 1:(n-1)) {
-        cov.x[k] <- (1/n)*sum((x[1:(n-k)] - mx)*(x[(1+k):n] - mx))
-    }
+    cov.x <- numeric(n-1)
+    for (k in kk)
+        cov.x[k] <- sum((x[1:(n-k)] - mx)*(x[(1+k):n] - mx)) / n
+
     M <- trunc(n^beta)
-    Im <- trunc(M/2)
+    M2 <- M %/% 2
     pw <- numeric(n-1)
-    for (k in 1:(n-1)) {
+    for (k in kk) {
         A_k <- k/M
         pw[k] <-
-            if (k <= Im)  1 - 6*A_k^2 *(1 - A_k)
+            if (k <= M2)  1 - 6*A_k^2 *(1 - A_k)
             else if (k <= M) 2*(1 - A_k)^3 else 0
     }
     periodogram <- numeric(g)
-    for (i in 1:g) {
-        periodogram[i] <- (1/(2*pi))*(var.x + 2*sum(cov.x*pw*cos(w[i]*(1:(n-1)))))
-    }
+    for (i in 1:g) # unscaled (will scale below)
+        periodogram[i] <- var.x + 2*sum(cov.x* pw * cos(w[i]*kk))
+
     pos <- j[periodogram > 0]
-    y.reg <- log(periodogram[pos])
+    y.reg <- log(periodogram[pos] / (2*pi))
     x.reg <- 2*log(2*sin(w[pos]/2)) ## = log( (2*sin(..)) ^ 2)
     fit <- lm(y.reg ~ x.reg)
     d.GPH <- coef(fit)[2]
     names(d.GPH) <- NULL
     x.r2 <- sum((x.reg - mean(x.reg))^2)
     var.d <- (0.539285*M/n)/ x.r2
-    var.reg <- sum((y.reg - fitted(fit))^2) / ((g - 1) * x.r2)
+    var.reg <- sum(resid(fit)^2) / ((g - 1) * x.r2)
     list(d = -d.GPH, sd.as = sqrt(var.d), sd.reg = sqrt(var.reg))
 }
 
