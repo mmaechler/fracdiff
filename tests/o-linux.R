bab986103ba7559f3525cddcd60a1e903281816e
Martin Maechler <maechler@r-project.org>
2006-09-08 10:58:57 +0000

    version of 2006.09.08.10.58.57 (now is 06 Sep 2018).

--- tests/o-linux.R
+++ tests/o-linux.R
@@ -10,6 +10,8 @@ options(digits = 10)
 
 x1 <-  fracdiff.sim( 5000, ar = .2, ma = -.4, d = .3, n.start=0, allow.0 = TRUE)
 (fd1 <- fracdiff(x1$series, nar = 1, nma = 1, dtol = 1e-10))
+vcov(fd1)
+logLik(fd1)
 
 fdCOVcomp <-
     c("h", "covariance.dpq", "stderror.dpq", "correlation.dpq", "hessian.dpq")
@@ -21,7 +23,8 @@ sapply(fd1u[fdCOVcomp], signif, digits= 8)
 ## 2)
 
 x2 <-  fracdiff.sim( 2048, ar = .8, ma = -.4, d = .3, n.start=0, allow.0 = TRUE)# -> NA's and problems
-(fd2 <- fracdiff(x2$series, nar = length(x2$ar), nma = length(x2$ma)))
+fd2 <- fracdiff(x2$series, nar = length(x2$ar), nma = length(x2$ma))
+summary(fd2)
 
 (fd2. <- fracdiff.var(x2$series, fd2, h = fd2$h / 8))[fdCOVcomp]
 (fd2u <- fracdiff.var(x2$series, fd2, h = fd2$h * 8))[fdCOVcomp]
