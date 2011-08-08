00ef0b4efd7bde4685a513aca3c8c4b25994094e
Martin Maechler <maechler@r-project.org>
2011-08-08 16:34:21 +0000

    version of 2011.08.08.16.34.21 (now is 06 Sep 2018).

--- src/fracdiff.h
+++ src/fracdiff.h
@@ -15,13 +15,12 @@ void fdsim(int *n, int *ip, int *iq, double *ar, double *ma,
 	   double *d__, double *mu, double *y, double *s,
 	   double *flmin, double *flmax, double *epmin, double *epmax);
 
-int lmder1_(S_fp fcn, int *m, int *n, double *x,
-	    double *fvec, double *fjac, int *ldfjac, double *ftol,
-	    double *xtol, double *gtol, int *maxfev, double *diag,
-	    int *mode, double *factor, int *info,
-	    int *nfev, int *njev, int *ipvt, double *qtf,
-	    double *wa1, double *wa2, double *wa3, double *wa4,
-	    double *y);
+double lmder1(S_fp fcn, int m, int n,
+	      double *x, double *fvec, double *fjac, int ldfjac,
+	      double *ftol, double *xtol, double *gtol, int *maxfev, double *diag,
+	      int *mode, double *factor, int *info,
+	      int *nfev, int *njev, int *ipvt, double *qtf,
+	      double *wa1, double *wa2, double *wa3, double *wa4, double *y);
 
 int ajqp_(double *qp, double *a, double *ajac,
 	  int *lajac, int *iflag, double *y);
