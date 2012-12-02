20ceeb4d47c69fee28033a9edec3357a5876b2a6
Martin Maechler <maechler@r-project.org>
2012-12-02 00:17:43 +0000

    version of 2012.12.02.00.17.43 (now is 06 Sep 2018).

--- tests/sim-2.R
+++ tests/sim-2.R
@@ -10,6 +10,7 @@ n.st <- 10
 
 set.seed(1)
 for(i in 1:200) {
+    cat(sprintf("%3d ", i))
     p <- sample(0:length(AR), 1)
     q <- sample(0:length(MA), 1)
     .ar <- AR[seq_len(p)]
@@ -20,6 +21,14 @@ for(i in 1:200) {
     y1 <- arima.sim(list(ar = .ar, ma = .ma), n = n, n.start = n.st, sd = sd)
     set.seed(sid)
     y2 <- fracdiff.sim(n = n, ar = .ar, ma = - .ma, d = 0,
-                       n.start = n.st, sd = sd)$series
-
+                       n.start = n.st, sd = sd, backComp = FALSE)
+    if(!isTRUE(aeq <- all.equal(c(y1), y2$series, tol = 1e-15)))
+        cat("y1 and y2 are not equal: ", aeq,"\n")
+    if(!(i %% 10))    cat("\n")
 }
+
+
+## Last Line:
+cat('Time elapsed: ', proc.time(),'\n')
+
+
