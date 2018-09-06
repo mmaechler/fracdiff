9578a0400fff43737084ad7264b8702debf27865
Martin Maechler <maechler@r-project.org>
2018-09-06 15:21:36 +0000

    version of 2018.09.06.15.21.36 (now is 06 Sep 2018).

--- R/fdGPH.R
+++ R/fdGPH.R
@@ -1,8 +1,8 @@
-#### by Valderio Reisen  -- Dec.2005
+#### by Valderio Reisen  -- Dec.2005-- $Id$
 
 ## MM(FIXME): This is "in parallel"  to Sperio() , see ./Sperio.R
 
-GPH <- function(x, bandw.exp = 0.5)
+fdGPH <- function(x, bandw.exp = 0.5)
 {
     x <- as.numeric(na.fail(as.ts(x)))
     if (any(is.na(x))) stop("NAs in x")
@@ -11,26 +11,27 @@ GPH <- function(x, bandw.exp = 0.5)
     ## Compute "smoothed" periodogram -- MM (FIXME): use  spec.pgram() !
     g <- trunc(n^bandw.exp)
     j <- 1:g
+    kk <- 1:(n-1)
     w <- 2*pi*j/n
     mx <- mean(x)
     var.x <- sum((x - mx)^2)/n # not /(n-1)
     cov.x <- numeric(n-1)
-    for (k in 1:(n-1)) {
-        cov.x[k] <- (1/n)*sum((x[1:(n-k)] - mx)*(x[(1+k):n] - mx))
-    }
+    for (k in kk)
+        cov.x[k] <- sum((x[1:(n-k)] - mx)*(x[(1+k):n] - mx)) / n
+
     periodogram <- numeric(g)
-    for (i in 1:g) {
-        periodogram[i] <- (1/(2*pi))*(var.x + 2*sum(cov.x*cos(w[i]*(1:(n-1)))))
-    }
+    for (i in 1:g) # unscaled (will scale below)
+        periodogram[i] <- var.x + 2*sum(cov.x * cos(w[i]*kk))
+
     pos <- j[periodogram > 0]
-    y.reg <- log(periodogram[pos])
+    y.reg <- log(periodogram[pos] / (2*pi))
     x.reg <- 2*log(2*sin(w[pos]/2)) ## = log( (2*sin(..)) ^ 2)
     fit <- lm(y.reg ~ x.reg)
     d.GPH <- coef(fit)[2]
     names(d.GPH) <- NULL
     x.r2 <- sum((x.reg - mean(x.reg))^2)
     var.d <- pi^2 / (6*x.r2)
-    var.reg <- sum( (y.reg - fitted(fit))^2 ) / ((g - 1) * x.r2)
+    var.reg <- sum(resid(fit)^2) / ((g - 1) * x.r2)
     ## return
     list(d = -d.GPH, sd.as = sqrt(var.d), sd.reg = sqrt(var.reg))
 }
