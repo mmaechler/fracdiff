fd03938cb086a0199f32c7352b2250aac9668947
Martin Maechler <maechler@r-project.org>
2018-09-07 22:31:06 +0200

    lm.fit instead of lm(), other cosmetics

--- R/fdGPH.R
+++ R/fdGPH.R
@@ -5,20 +5,20 @@
 
 fdGPH <- function(x, bandw.exp = 0.5)
 {
+    if(NCOL(x) > 1) stop("only implemented for univariate time series")
     x <- as.numeric(na.fail(as.ts(x)))
     if (any(is.na(x))) stop("NAs in x")
-    if (NCOL(x) > 1) stop("only implemented for univariate time series")
     n <- length(x)
     ## Compute "smoothed" periodogram -- MM (FIXME): use  spec.pgram() !
     g <- trunc(n^bandw.exp)
     j <- 1:g
     kk <- 1:(n-1)
     w <- 2*pi*j/n
-    mx <- mean(x)
-    var.x <- sum((x - mx)^2)/n # not /(n-1)
-    cov.x <- numeric(n-1)
+    x <- x - mean(x)
+    var.x <- sum(x^2)/n # not /(n-1)
+    cov.x <- numeric(n-1L)
     for (k in kk)
-        cov.x[k] <- sum((x[1:(n-k)] - mx)*(x[(1+k):n] - mx)) / n
+        cov.x[k] <- sum(x[1:(n-k)] * x[(1+k):n]) / n
 
     periodogram <- numeric(g)
     for (i in 1:g) # unscaled (will scale below)
@@ -27,8 +27,8 @@ fdGPH <- function(x, bandw.exp = 0.5)
     pos <- j[periodogram > 0]
     y.reg <- log(periodogram[pos] / (2*pi))
     x.reg <- 2*log(2*sin(w[pos]/2)) ## = log( (2*sin(..)) ^ 2)
-    fit <- lm(y.reg ~ x.reg)
-    d.GPH <- coef(fit)[[2]]
+    fit <- lm.fit(cbind(1, x.reg), y.reg)
+    d.GPH <- coef(fit)[["x.reg"]]
     x.r2 <- sum((x.reg - mean(x.reg))^2)
     var.d <- pi^2 / (6*x.r2)
     var.reg <- sum(resid(fit)^2) / ((g - 1) * x.r2)
--- R/fdSperio.R
+++ R/fdSperio.R
@@ -1,23 +1,24 @@
-#### by Valderio Reisen  -- Dec.2005-- $Id$
+#### by Valderio Reisen  -- Dec.2005--
+#### Tweaks by MM
 
 ## MM(FIXME): This is "in parallel"  to fdGPH() , see ./fdGPH.R
 
 fdSperio <- function(x, bandw.exp = 0.5, beta = 0.9)
 {
+    if(NCOL(x) > 1) stop("only implemented for univariate time series")
     x <- as.numeric(na.fail(as.ts(x)))
     if (any(is.na(x))) stop("NAs in x")
-    if (NCOL(x) > 1) stop("only implemented for univariate time series")
     n <- length(x)
     ## Compute "smoothed" periodogram -- MM (FIXME): use  spec.pgram() !
     g <- trunc(n^bandw.exp)
     j <- 1:g
     kk <- 1:(n-1)
     w <- 2*pi*j/n
-    mx <- mean(x)
-    var.x <- sum((x - mx)^2)/n # not /(n-1)
+    x <- x - mean(x)
+    var.x <- sum(x^2)/n # not /(n-1)
     cov.x <- numeric(n-1)
     for (k in kk)
-        cov.x[k] <- sum((x[1:(n-k)] - mx)*(x[(1+k):n] - mx)) / n
+        cov.x[k] <- sum(x[1:(n-k)] * x[(1+k):n]) / n
 
     M <- trunc(n^beta)
     M2 <- M %/% 2
@@ -35,8 +36,8 @@ fdSperio <- function(x, bandw.exp = 0.5, beta = 0.9)
     pos <- j[periodogram > 0]
     y.reg <- log(periodogram[pos] / (2*pi))
     x.reg <- 2*log(2*sin(w[pos]/2)) ## = log( (2*sin(..)) ^ 2)
-    fit <- lm(y.reg ~ x.reg)
-    d.GPH <- coef(fit)[[2]]
+    fit <- lm.fit(cbind(1, x.reg), y.reg)
+    d.GPH <- coef(fit)[["x.reg"]]
     x.r2 <- sum((x.reg - mean(x.reg))^2)
     var.d <- (0.539285*M/n)/ x.r2
     var.reg <- sum(resid(fit)^2) / ((g - 1) * x.r2)
