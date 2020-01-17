65b847315c65e252bbf0ebfe44e372cbeeeb3df0
Martin Maechler <maechler@r-project.org>
2020-01-17 17:53:02 +0100

    stopifnot( all.equal(..) )  instead of output (w/ small diffs)
    * DESCRIPTION (Version): 1.5-1; for CRAN 'Additional issues', fiddling w/
    * tests/Valderio-ex.R: show all.equal(*, tol=0) only if "surprising"

--- tests/ex.R
+++ tests/ex.R
@@ -15,9 +15,42 @@ logLik(fd1)
 fdCOVcomp <-
     c("h", "covariance.dpq", "stderror.dpq", "correlation.dpq", "hessian.dpq")
 fd1. <- fracdiff.var(x1$series, fd1, h = fd1$h / 2)
-sapply(fd1.[fdCOVcomp], signif, digits = 4)
+dns <- dimnames(fd1.$covariance.dpq)
+
+## dput(sapply(fd1.[fdCOVcomp], signif, digits = 4)) # edited:
+fd1.L <- list(
+    h = 3.7155e-05,
+    covariance.dpq = matrix(c(0.0005966, -0.0008052, -0.0001897,
+                              -0.0008052, 0.001612, 0.000664,
+                              -0.0001897, 0.000664, 0.0005485),
+                            3L, 3L, dimnames = dns),
+    stderror.dpq = c(0.02443, 0.04015, 0.02342),
+    correlation.dpq = matrix(c(1, -0.821, -0.3316,
+                               -0.821, 1,  0.7061,
+                               -0.3316, 0.7061, 1), 3),
+    hessian.dpq = matrix(c(-8252, -5875, 4258,
+                           -5875, -5420, 4529,
+                           4258, 4529, -5834),
+                         3L, 3L, dimnames = dns))
+stopifnot(all.equal(fd1.[fdCOVcomp], fd1.L, tolerance = 2e-4))
+
 fd1u <- fracdiff.var(x1$series, fd1, h = fd1$h * 8)
-sapply(fd1u[fdCOVcomp], signif, digits = 4)
+## dput(sapply(fd1u[fdCOVcomp], signif, digits = 4)) :
+fd1uL <- list(
+    h = 0.0005945,
+    covariance.dpq = matrix(c(0.0005966, -0.0008052, -0.0001897,
+                              -0.0008052, 0.001612, 0.000664,
+                              -0.0001897, 0.000664, 0.0005485),
+                            3L, 3L, dimnames = dns),
+    stderror.dpq = c(0.02443, 0.04015, 0.02342),
+    correlation.dpq = matrix(c(1, -0.821, -0.3316,
+                               -0.821, 1, 0.7061,
+                               -0.3316, 0.7061, 1), 3),
+    hessian.dpq = matrix(c(-8252, -5875, 4258,
+                           -5875, -5420, 4529,
+                            4258,  4529, -5834),
+                            3L, 3L, dimnames = dns))
+stopifnot( all.equal(fd1u[fdCOVcomp], fd1uL, tolerance = 2e-4) )
 
 ## 2)
 
